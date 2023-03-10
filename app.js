e8c4d5934296544c84ed43bf7f95f75b351a306b
TeaveLeaf <116187490+TeaveLeaf@users.noreply.github.com>
2023-03-10 08:17:03 -0800

    KC - Made working start screen

--- app.js
+++ app.js
@@ -4,10 +4,15 @@
 
 //function makes thingy appear and or disappear
 function visibility() {
-    var x = document.getElementById("wha");
+    var x = document.getElementById("startgm");
+    var y = document.getElementById("playerbrd");
+    var z = document.getElementById("board");
     if (x.style.display === "none") {
       x.style.display = "block";
+      y.style.display = "none";
     } else {
       x.style.display = "none";
+      y.style.display = "flex";
+      z.style.display = "flex";
     }
   }
