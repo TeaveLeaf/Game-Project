1d6e5d8c03c872ad8fc415e186ac9e6a9af30ba2
TeaveLeaf <kironashuexploded@gmail.com>
2023-03-19 20:32:27 -0700

    KC - Trying to do gameboard

--- app.js
+++ app.js
@@ -1,13 +1,57 @@
+function setup() {
+  create(400,400);
+
+  let resolution = 100;
+  let cols = width / resolution;
+  let rows = height / resolution;
+  
+  let x = 0;
+  let y = (rows-1)*resolution;
+  for (let i = 0; i < cols * rows; i++) {
+    let tile = new Tile(100, 100, resolution, i + 1);
+    tiles.push(tile);
+    x = x + (resolution * dir);
+    if (x >= width) {
+      x -= resolution;
+      y -= resolution;
+      dir *= -1;
+    }
+    if (x <= 0) {
+      x = resolution;
+      y -= resolution;
+      dir *= -1;
+    }
+  }
 
 
-let boardData = [
-  [0, 0, 0],
-  [0, 0, 0],
-  [0, 0, 0]
-];
+}
+
+function draw() {
+  background(51);
+
+  for (let tile of tiles){
+    tile.show();
+  }
+
+}
 
 
-console.log(boardData);
+
+
+
+
+
+// let boardData = [
+//   [0, 0, 0],
+//   [0, 0, 0],
+//   [0, 0, 0]
+// ];
+
+
+
+// var boardIndex = 0;
+
+
 
 
 
@@ -28,7 +72,7 @@ function visibility() {
 }
 
 //Dice roll function
-  function diceRoll(){
+function diceRoll(){
     var num;
     num = Math.floor(Math.random()*6)+1;
     document.getElementById("diceShow2").innerHTML = num;
@@ -116,3 +160,15 @@ function cardDeck(){
 // const cellElements = document.getElementsByClassName(".cell");
 
 // console.log(boardData);
+
+
+
+// const boardGame = document.querySelector('#testBoard')
+// const SQUARES_NUMBER = 648
+
+// for (let i = 0; i < SQUARES_NUMBER; i ++) {
+//   const square = document.createElement('div')
+//   square.classList.add('square')
+
+//   square.addEventListener('mouseover', () => setColor)
+// }
