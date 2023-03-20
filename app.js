9c0cb5617f41d95dfaa70808e095ead087302e7d
TeaveLeaf <116187490+TeaveLeaf@users.noreply.github.com>
2023-03-20 10:43:34 -0700

    KC - Working on dice

--- app.js
+++ app.js
@@ -1,12 +1,64 @@
 let boardData = [
-  [0, 0, 0],
-  [0, 0, 0],
+  [1, 0, 0],
+  [0, 2, 0],
   [0, 0, 0]
 ];
 
+let start = 1;
+let block = 2;
+let purple = boardData[0][0];
+let orange = boardData[0][0];
 
 
 
+
+var num;
+//Dice roll function
+function diceRoll(){
+  num = Math.floor(Math.random()*6)+1;
+  document.getElementById("diceShow2").innerHTML = num;
+  console.log("Dice Rolled");
+  console.log("Dice got: " + num);
+  movement();
+}
+
+
+function movement () {
+  let x = 0;
+  let y = 0;
+  var soe;
+  for (let i = 0; i < num; i++) {
+    if(x < 2) {
+      x = x + 1;
+    } else if(x == 2) {
+      x = x
+    }
+    if(y == 0){
+      y = y + 1
+      // if(num = 1){
+      //   y = y + 1;
+      // } else if(y == 1) {
+      //   y = y + 1;
+      // }
+      // if(num = 1){
+      //   if(y == 0){
+      //     y = y + 1;
+      //   }
+      // }
+    } else if(y == 1) {
+      y = y + 1;
+    } else if(y == 2) {
+      y = y
+      soe = soe + 1;
+      if(soe == 2) {
+        soe = 0;
+        y = - 1;
+      }
+    }
+  }
+  purple = boardData[x][y];
+  console.log("Purple's position is: " + "(" + x + "," + y + ")");
+}
 
 
 
@@ -14,27 +66,20 @@ let boardData = [
 
 //Function makes thingy appear and or disappear
 function visibility() {
-    var x = document.getElementById("startgm");
-    var y = document.getElementById("playerbrd");
-    var z = document.getElementById("board");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-      y.style.display = "none";
+    var start = document.getElementById("startgm");
+    var playerBrd = document.getElementById("playerbrd");
+    var board = document.getElementById("board");
+    if (start.style.display === "none") {
+      start.style.display = "block";
+      board.style.display = "none";
     } else {
-      x.style.display = "none";
-      y.style.display = "flex";
-      z.style.display = "block";
+      start.style.display = "none";
+      playerBrd.style.display = "flex";
+      board.style.display = "block";
     }
     console.log("Game started");
 }
 
-//Dice roll function
-function diceRoll(){
-    var num;
-    num = Math.floor(Math.random()*6)+1;
-    document.getElementById("diceShow2").innerHTML = num;
-    console.log("Dice Rolled");
-}
 
 
 
@@ -92,7 +137,7 @@ function cardDeck(){
 // different ways ive tried making the gameboard that did not work
 
 
-//codes r mostly from tuts, idk why i am not understanding
+//codes r mostly from tuts
 // let player = 1;
 
 // const cellElements = document.querySelectorAll(".cell");
