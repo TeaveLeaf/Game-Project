b1ecd67e66d56d5c9ea638706246c2356270d204
TeaveLeaf <116187490+TeaveLeaf@users.noreply.github.com>
2023-03-21 10:04:53 -0700

    KC - Working on movement

--- app.js
+++ app.js
@@ -1,26 +1,53 @@
-// let boardData = [
-//   [1, 0, 0],
-//   [0, 2, 0],
-//   [0, 0, 0]
+
+
+// let boardNum = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9]
 // ];
 
-let boardData = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9]
-];
 
-let start = 1;
-let block = 2;
+// let boardData = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9]
+// ];
+
+// let start = 1;
+// let block = 2;
 // let purple = boardData[0][0];
 // let orange = boardData[0][0];
 
-let purpMove;
+// let boardTop = [0, 0, 0];
+// let boardSides = [0, 1, 0];
+// let boardBottom = [0, 0, 0];
 
+let boardData = [3,0,0,0,0,0,0,0,0];
+
+let multPlays = 3;
+let purple = 1;
+let orang = 2;
+
+let purpData = 0; //Start at (0,0)
+let orangData = 0;
+
+let purpMove;
+let purPos = 0;
+
+let turns = [0, 1];
+
+let c1 = document.getElementById("cell1");
+let c2 = document.getElementById("cell2");
+let c3 = document.getElementById("cell3");
+let c4 = document.getElementById("cell4");
+let block = document.getElementById("block");
+let c5 = document.getElementById("cell5");
+let c6 = document.getElementById("cell6");
+let c7 = document.getElementById("cell7");
+let c8 = document.getElementById("cell8");
 
 //Test to see if it actually shows the data -- works
 console.log(boardData[2][2]);
-
 
 var num;
 //Dice roll function
@@ -29,61 +56,65 @@ function diceRoll(){
   document.getElementById("diceShow2").innerHTML = num;
   console.log("Dice Rolled");
   console.log("Dice got: " + num);
-  purpMove = num;
-  console.log("Purple needs to move: " + purpMove);
-  move();
 }
 
-function move () {
-  let first = 0;
-  let sec = 0;
-  // if(purpMove <= 5) {
-  //   for (let i = 0; i < 2; i++) {
-  //     first = first + 1;
-  //     console.log("WA" + first);
-  //   }
-  // }
+function movement() {
+  purpPos = boardData[num];
 }
 
-
-function movement () {
-  let x = 0;
-  let y = 0;
-  var soe;
-  for (let i = 0; i < num; i++) {
-    if(x < 2) {
-      x = x + 1;
-    } else if(x == 2) {
-      x = x
-    }
-    if(y == 0){
-      y = y + 1
-      // if(num = 1){
-      //   y = y + 1;
-      // } else if(y == 1) {
-      //   y = y + 1;
-      // }
-      // if(num = 1){
-      //   if(y == 0){
-      //     y = y + 1;
-      //   }
-      // }
-    } else if(y == 1) {
-      y = y + 1;
-    } else if(y == 2) {
-      y = y
-      soe = soe + 1;
-      if(soe == 2) {
-        soe = 0;
-        y = - 1;
-      }
-    }
+function updateBoard() {
+  if(boardData[0] == 3){
+    document.getElementById("cell1").className = "cell purorang";
+  } else if(boardData[2] == 3){
+    document.getElementById("cell2").className = "cell purorang";
+  } else if(boardData[3] == 3){
+    document.getElementById("cell3").className = "cell purorang";
+  } else if(boardData[4] == 3){
+    document.getElementById("cell4").className = "cell purorang";
+  } else if(boardData[5] == 3){
+    document.getElementById("cell5").className = "cell purorang";
+  } else if(boardData[6] == 3){
+    document.getElementById("cell6").className = "cell purorang";
+  } else if(boardData[7] == 3){
+    document.getElementById("cell7").className = "cell purorang";
+  } else if(boardData[8] == 3){
+    document.getElementById("cell8").className = "cell purorang";
   }
-  purple = boardData[x][y];
-  console.log("Purple's position is: " + "(" + x + "," + y + ")");
+  if(boardData[1] == 1){
+    document.getElementById("cell1").className = "cell purp";
+  } else if(boardData[2] == 1){
+    document.getElementById("cell2").className = "cell purp";
+  } else if(boardData[3] == 1){
+    document.getElementById("cell3").className = "cell purp";
+  } else if(boardData[4] == 1){
+    document.getElementById("cell4").className = "cell purp";
+  } else if(boardData[5] == 1){
+    document.getElementById("cell5").className = "cell purp";
+  } else if(boardData[6] == 1){
+    document.getElementById("cell6").className = "cell purp";
+  } else if(boardData[7] == 1){
+    document.getElementById("cell7").className = "cell purp";
+  } else if(boardData[8] == 1){
+    document.getElementById("cell8").className = "cell purp";
+  }
+  if(boardData[1] == 2){
+    document.getElementById("cell1").className = "cell orang";
+  } else if(boardData[2] == 2){
+    document.getElementById("cell2").className = "cell orang";
+  } else if(boardData[3] == 2){
+    document.getElementById("cell3").className = "cell orang";
+  } else if(boardData[4] == 2){
+    document.getElementById("cell4").className = "cell orang";
+  } else if(boardData[5] == 2){
+    document.getElementById("cell5").className = "cell orang";
+  } else if(boardData[6] == 2){
+    document.getElementById("cell6").className = "cell orang";
+  } else if(boardData[7] == 2){
+    document.getElementById("cell7").className = "cell orang";
+  } else if(boardData[8] == 2){
+    document.getElementById("cell8").className = "cell orang";
+  }
 }
-
-
 
 
 
@@ -101,6 +132,7 @@ function visibility() {
       board.style.display = "block";
     }
     console.log("Game started");
+    updateBoard();
 }
 
 
@@ -260,3 +292,124 @@ function cardDeck(){
 // ];
 
 // objectTest.data = stuff;    what ?? D:
+
+
+//Movement
+// function move () { //maybe my logic is wrong and i need to do it a diff way ??
+//   let timesPlayed;
+//   timesPlayed = timesPlayed + 1;
+//   let first = 0;
+//   let second = 0;
+
+//   let firstA = 0;
+//   let secondA = 0;
+
+//   if (timesPlayed >= 1){
+//     first = bow;
+//     second = wow;
+//   }
+  
+//   for (let i = 0; i < purpMove; i++) {
+//     if (second <= 0) { //x increases first
+//       first = first + 1;
+//       console.log("FIRST " + first);
+//     }
+//     // firstA = firstA + 1;
+//     if(first == 2){ //x stops at 2
+//       first = first + 0;
+//       firstA = first + 1;
+//       // firstA = firstA + 0;
+//       // second = second + 1;
+//       // secondA = secondA + 1;
+//       console.log("Test 1: " + first);
+//     }
+//     if(firstA == 4){ //y will start increasing when firstA hits 3
+//       first = first + 0;
+//       firstA = firstA + 0;
+//       second = second + 1;
+//       // secondA = secondA + 0;
+//       console.log("Test 2: " + second);
+//     }
+//     if(second == 2){ //y will stop increasing when it hits 2
+//       second = second + 0;
+//       secondA = second + 1;
+//       // secondA = secondA + 0;
+//       console.log("Test 3: " + second);
+//     }
+//     if(first == 2 && secondA == 4) { //x will decrease after y
+//       first = first - 1;
+//       firstA = firstA - 1;
+//       // firstA = firstA - 1;
+//       console.log("Test 4: " + first);
+//     }
+//     if(first == 0 && firstA == 0) {
+//       second = second - 1;
+//       console.log("Test 5: " + second);
+//     }
+//   } //for loop end
+
+//   let bow = first;
+//   let wow = second;
+//   console.log(first);
+//   console.log(second);
+// // console.log("Purple's position is: " + "(" + first + "," + second + ")");
+// }
+
+
+// function movement () {
+//   let x = 0;
+//   let y = 0;
+//   var soe;
+//   for (let i = 0; i < num; i++) {
+//     if(x < 2) {
+//       x = x + 1;
+//     } else if(x == 2) {
+//       x = x
+//     }
+//     if(y == 0){
+//       y = y + 1
+//       // if(num = 1){
+//       //   y = y + 1;
+//       // } else if(y == 1) {
+//       //   y = y + 1;
+//       // }
+//       // if(num = 1){
+//       //   if(y == 0){
+//       //     y = y + 1;
+//       //   }
+//       // }
+//     } else if(y == 1) {
+//       y = y + 1;
+//     } else if(y == 2) {
+//       y = y
+//       soe = soe + 1;
+//       if(soe == 2) {
+//         soe = 0;
+//         y = - 1;
+//       }
+//     }
+//   }
+//   purple = boardData[x][y];
+//   console.log("Purple's position is: " + "(" + x + "," + y + ")");
+// }
+
+
+
+// function purpleMove() {
+//   let porple = 0;
+//   // for (let i = 0; i < purpMove; i++) {
+//   //   porple = porple + 1;
+//   // }
+//   purpMove = num + porple;
+//   console.log("Purple needs to move: " + purpMove);
+//   purSpace = purSpace + purpMove;
+//   for (let i = 0; i < purSpace; i++) {
+//     boardNum = boardNum[i];
+//     if(boardNum > 6) {
+//       boardNum = boardNum[0];
+//       purSpace = 0;
+//     }
+//   }
+//   console.log(purSpace);
+//   console.log(boardNum);
+// }
