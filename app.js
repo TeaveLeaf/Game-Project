0478e9105902ae50f632814c89e621019bb62e7f
TeaveLeaf <116187490+TeaveLeaf@users.noreply.github.com>
2023-03-22 10:13:21 -0700

    KC - Movement + turns down for both players

--- app.js
+++ app.js
@@ -31,10 +31,14 @@ let boardData = [1,2,3,4,5,6,7,8];
 let purpData = 0;
 let orangData = 0;
 
-let purpMove = 0;
+let purpMove = 1;
 let purPos = 0;
 
-let turns = [0, 1];
+let orangMove = 1;
+
+let toge = 0;
+
+let turns = 0;
 
 let c1 = document.getElementById("cell1");
 let c2 = document.getElementById("cell2");
@@ -47,16 +51,25 @@ let c7 = document.getElementById("cell7");
 let c8 = document.getElementById("cell8");
 
 
+
+
+
 var num;
 //Dice roll function
 function diceRoll(){
   num = Math.floor(Math.random()*6)+1;
   document.getElementById("diceShow2").innerHTML = num;
   console.log("Dice rolled a " + num);
-  movement();
+  if(turns == 0){
+    console.log("OURPLE");
+    Pmovement();
+  } else if(turns == 1){
+    console.log("ORANGE");
+    Omovement();
+  }
 }
 
-function movement() { //WORKS IT WORKS MOSTLY WOOHOO
+function Pmovement() { //WORKS IT WORKS MOSTLY WOOHOO
   // console.log("hihi the num is " + num);
   // console.log("purpmove is " + purpMove);
   let testy = 0;
@@ -67,20 +80,63 @@ function movement() { //WORKS IT WORKS MOSTLY WOOHOO
       testy = testy + 0;
       console.log("A testy is " + testy);
       console.log("A pm is " + purpMove);
+      updateBoard();
     } else if(testy == num && purpMove >= 9) { //if testy = num and bigger than 9 pm = 0
       purpMove = 1;
       testy = 0;
       console.log("B testy is " + testy);
       console.log("B pm is " + purpMove);
+      updateBoard();
     } else { //if not equal to num just add
       purpMove = purpMove + 1;
       testy = testy + 1;
       console.log("C testy is " + testy);
       console.log("C pm is " + purpMove);
+      updateBoard();
     }
   }
   console.log("Purple has moved");
+  console.log("Purple's turn over");
+  turns = 1;
   updateBoard();
+}
+
+
+function Omovement() { //WORKS IT WORKS MOSTLY WOOHOO
+  // console.log("hihi the num is " + num);
+  // console.log("purpmove is " + purpMove);
+  let testy2 = 0;
+  // console.log("testy is " + testy);
+  for (let i = 0; i < 8; i++){
+    if(testy2 == num && orangMove < 9) { //if testy is equal to num and pm less than 9 no add
+      orangMove = orangMove + 0;
+      testy2 = testy2 + 0;
+      console.log("A testy is " + testy2);
+      console.log("A pm is " + orangMove);
+      updateBoard();
+    } else if(testy2 == num && orangMove >= 9) { //if testy = num and bigger than 9 pm = 0
+      orangMove = 1;
+      testy2 = 0;
+      console.log("B testy is " + testy2);
+      console.log("B pm is " + orangMove);
+      updateBoard();
+    } else { //if not equal to num just add
+      orangMove = orangMove + 1;
+      testy2 = testy2 + 1;
+      console.log("C testy is " + testy2);
+      console.log("C pm is " + orangMove);
+      updateBoard();
+    }
+  }
+  console.log("Orange has moved");
+  console.log("Orange's turn over");
+  turns = 0;
+  updateBoard();
+}
+
+//Draw card function yay oh wow woohoo so fun
+function drawCard() {
+  randoDraw = Math.floor(Math.random()*6)+1;
 }
 
 
@@ -113,6 +169,42 @@ function updateBoard() {
   } else if (purpMove == 8){
     document.getElementById("cell8").className = "cell purpal";
   } else document.getElementsByClassName("cell").className = "cell";
+
+  if (orangMove == 1){
+    document.getElementById("cell1").className = "cell orang";
+  } else if (orangMove == 2){
+    document.getElementById("cell2").className = "cell orang";
+  } else if (orangMove == 3){
+    document.getElementById("cell3").className = "cell orang";
+  } else if (orangMove == 4){
+    document.getElementById("cell4").className = "cell orang";
+  } else if (orangMove == 5){
+    document.getElementById("cell5").className = "cell orang";
+  } else if (orangMove == 6){
+    document.getElementById("cell6").className = "cell orang";
+  } else if (orangMove == 7){
+    document.getElementById("cell7").className = "cell orang";
+  } else if (orangMove == 8){
+    document.getElementById("cell8").className = "cell orang";
+  } else document.getElementsByClassName("cell").className = "cell";
+
+  if(purpMove == 1 && orangMove == 1) {
+    document.getElementById("cell1").className = "cell purorang";
+  } else if(purpMove == 2 && orangMove == 2) {
+    document.getElementById("cell2").className = "cell purorang";
+  } else if(purpMove == 3 && orangMove == 3) {
+    document.getElementById("cell3").className = "cell purorang";
+  } else if(purpMove == 3 && orangMove == 3) {
+    document.getElementById("cell3").className = "cell purorang";
+  } else if(purpMove == 3 && orangMove == 3) {
+    document.getElementById("cell3").className = "cell purorang";
+  } else if(purpMove == 3 && orangMove == 3) {
+    document.getElementById("cell3").className = "cell purorang";
+  } else if(purpMove == 3 && orangMove == 3) {
+    document.getElementById("cell3").className = "cell purorang";
+  } else if(purpMove == 3 && orangMove == 3) {
+    document.getElementById("cell3").className = "cell purorang";
+  }
 }
 
 
